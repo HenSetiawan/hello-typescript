5befa767afcb65aad3a460f064a293ab33b29104
hensetiawan <hendystyawan74@gmail.com>
2023-12-26 13:13:31 +0700

    hello function

--- test/isHello.test.ts
+++ test/isHello.test.ts
@@ -1,6 +1,9 @@
+import { sayHello } from "../src/hello";
+
 describe("hello", () => {
-    it("should say hello", () => {
-        const name = 'Hello Hendy';
-        expect(name).toBe('Hello Hendy');
+  it("should say hello", () => {
+    expect(sayHello("Hendy")).toBe("Hello Hendy");
+    expect(sayHello("Budi")).toBe("Hello Budi");
+    expect(sayHello("Joko")).toBe("Hello Joko");
   });
 });
