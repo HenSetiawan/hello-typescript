e8169d84daae24698c0dbbbb02a219a0d9d395df
hensetiawan <hendystyawan74@gmail.com>
2023-12-26 19:07:17 +0700

    data type basics

---
+++ test/dataType.test.ts
@@ -0,0 +1,12 @@
+describe("Data Type", function () {
+  it("should must declare", function () {
+    let name: string = "Hendy Setiawan";
+    let balance: number = 1000000;
+    let isVip: boolean = true;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+
+  });
+});
