3f9766335ec3d47b090da6fab18ab3dd6a22e497
hensetiawan <hendystyawan74@gmail.com>
2023-12-27 18:47:29 +0700

    enum type

---
+++ test/enum.test.ts
@@ -0,0 +1,23 @@
+describe("enum type", () => {
+  it("enum type", () => {
+    enum priority {
+      VIP,
+      VVIP,
+      REGULAR,
+    }
+    const Hendy: {
+      id: string | number;
+      name: string;
+      age: number;
+      type: priority;
+    } = {
+      id: 2023,
+      name: "Hendy",
+      age: 22,
+      type: priority.REGULAR,
+    };
+
+    expect(Hendy.name).toBe("Hendy");
+    expect(Hendy.age).toBe(22);
+  });
+});
