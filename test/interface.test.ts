84f321ff78bcfc69c8217081c6fb3eab45e2d467
hensetiawan <hendystyawan74@gmail.com>
2023-12-28 22:01:39 +0700

    extends interface

--- test/interface.test.ts
+++ test/interface.test.ts
@@ -8,6 +8,10 @@ describe("interface", () => {
       descrition?: string;
     }
 
+    interface employee extends person {
+      numberOfEmployee: number;
+    }
+
     interface addFunction {
       (firstValue: number, secondValue: number): number;
     }
@@ -19,13 +23,13 @@ describe("interface", () => {
       return firstValue + secondValue;
     };
 
-    const Hendy: person = {
+    const Hendy: employee = {
       id: 2201,
       name: "Hendy",
       age: 22,
       descrition: "hendy is person who write this code",
+      numberOfEmployee: 1,
     };
-
     expect(Hendy.name).toBe("Hendy");
     expect(Hendy.age).toBe(22);
     expect(addNumber(10, 10)).toBe(20);
