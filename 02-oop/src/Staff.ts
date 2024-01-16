3b1513fbd537372aa39039c25853fb37f7b1f92a
hensetiawan <hendystyawan74@gmail.com>
2024-01-16 19:09:36 +0700

    feat : polymorphism

---
+++ 02-oop/src/Staff.ts
@@ -0,0 +1,5 @@
+import { Employee } from "./Employee";
+
+class Staff extends Employee {}
+
+export { Staff };
--- 02-oop/test/hello.test.ts
+++ 02-oop/test/hello.test.ts
@@ -1,9 +1,14 @@
 import { Employee } from "../src/Employee";
+import { Staff } from "../src/Staff";
 
 describe("hello testing", () => {
   it("method should work", () => {
-    const hendy = new Employee(23, 22, "Hendy",'MIS');
-    expect(hendy.sayHello("Dimas")).toBe("hello Dimas my name is Hendy and im is employee here");
+    let hendy: Employee = new Employee(23, 22, "Hendy", "MIS");
+    hendy = new Staff(22, 22, "Hendy", "MIS");
+
+    expect(hendy.sayHello("Dimas")).toBe(
+      "hello Dimas my name is Hendy and im is employee here"
+    );
     hendy.name = "Setiawan";
     expect(hendy.sayHello("Dimas")).toBe(
       "hello Dimas my name is Setiawan and im is employee here"
